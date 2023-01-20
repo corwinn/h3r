8f6885953025bf9f06dd81432adcad54551ebbda
the wind <corwinn@users.noreply.github.com>
2023-01-20 10:07:12 +0200

    Async directory enumerator code; and repo setup

---
+++ game/h3r_gamearchives.h
@@ -0,0 +1,87 @@
+/**** BEGIN LICENSE BLOCK ****
+
+BSD 3-Clause License
+
+Copyright (c) 2021-2023, the wind.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+**** END LICENCE BLOCK ****/
+
+#ifndef _H3R_GAMEARCHIVES_H_
+#define _H3R_GAMEARCHIVES_H_
+
+#include "h3r.h"
+#include "h3r_string.h"
+#include "h3r_list.h"
+
+H3R_NAMESPACE
+
+#define public public:
+#define private private:
+
+// Game data archives state.
+// Also, because case sensitive FS requires specific handling.
+class GameArchives final
+{
+    public class Entry final
+    {
+        private bool _found {};
+        public List<String> Locations;
+        public String Name;
+        //NOTE do not forget the next time: conversion as well, so
+        //     make these explicit, to avoid the WTNiceMointainView moments
+        //TODO print "C++ Caveats" sheet and put it on the NiceMointainView wall
+        // public Entry(const String & name) : Name {name.ToLower ()} {}
+        public Entry(String && name) : Name{name} {}
+        public void Found() { _found = true; }
+        public bool operator==(const Entry & s) const { return s.Name == Name; }
+    };
+    public List<Entry> _ba;
+    public inline const List<Entry> & BaseArchives() const { return _ba; }
+    public GameArchives()
+        : _ba {List<Entry> {}
+            << String {"H3ab_ahd.snd"}.ToLower ()
+            << String {"H3ab_bmp.lod"}.ToLower ()
+            << String {"H3ab_ahd.vid"}.ToLower ()
+            << String {"H3ab_spr.lod"}.ToLower ()
+            << String {"Heroes3.snd" }.ToLower () // 2 of them
+            << String {"Heroes3.vid" }.ToLower ()
+            << String {"H3bitmap.lod"}.ToLower ()
+            << String {"VIDEO.VID"   }.ToLower ()
+            << String {"H3sprite.lod"}.ToLower ()}
+    {
+    }
+
+    // [MOD] public void Add(String archive); //LATER
+};// GameArchives
+
+#undef private
+#undef public
+
+NAMESPACE_H3R
+#endif
---
+++ utils/h3r_asyncfsenum.h
@@ -0,0 +1,137 @@
+/**** BEGIN LICENSE BLOCK ****
+
+BSD 3-Clause License
+
+Copyright (c) 2021-2023, the wind.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+**** END LICENCE BLOCK ****/
+
+#ifndef _H3R_ASYNCFSENUM_H_
+#define _H3R_ASYNCFSENUM_H_
+
+#include "h3r.h"
+#include "h3r_os.h"
+#include "h3r_string.h"
+#include "h3r_stack.h"
+#include "h3r_iasynctask.h"
+#include "h3r_taskstate.h"
+#include "h3r_taskthread.h"
+
+H3R_NAMESPACE
+
+#define public public:
+#define private private:
+
+// Recursive (directory) asynchronous FS enumerator.
+// T - observer;
+// bool (T::*)(const EnumItem &) - event handler called in async thread context;
+// EnumItem a.k.a. EventArgs:
+//   Name        - the full path name (starting with base_path)
+//   FileName    - just the file name
+//   IsDirectory - whether this is directory
+//   Progress    - the sequential number of the found file
+//   PercentageProgress () - false, because I have yet to find an FS that does
+//                           support file num per directory tree
+template <typename T> class AsyncFsEnum final
+{
+#undef public
+    public: class EnumItem final : public TaskState
+#define public public:
+    {
+        using TaskState::TaskState;
+        public inline virtual bool PercentageProgress() const { return false; }
+        public String Name {};
+        public String FileName {};
+        public bool IsDirectory {};
+        public int Progress {};
+    };
+    // since the notification is in the task thread context
+    using OnItem = bool (T::*)(const EnumItem &);
+    private T * _on_item_observer;
+    private OnItem _on_item;
+    private bool ThrNotify()
+    {
+        return (_on_item_observer->*_on_item) (_task.State);
+    }
+    private String _root;
+    private TaskThread _thread {};
+    public AsyncFsEnum(String base_path, T * observer, OnItem handle_on_item)
+        : _on_item_observer{observer}, _on_item{handle_on_item},
+        _root{base_path}, _task {*this}
+    {
+        _thread.Task = _task;
+    }
+
+#undef public
+    private class EnumTask final : public IAsyncTask
+#define public public:
+    {
+        private AsyncFsEnum<T> & _subject;
+        private bool _complete {false};
+        private Stack<String> _directories {};
+        private String _current_root {};
+        private size_t _stack_load {};
+        public EnumItem State;
+        public EnumTask(AsyncFsEnum<T> & subject)
+            : _subject{subject}, State {0, "Enumerating..."} {}
+        public inline TaskState Whatsup() override { return State; }
+        public inline void Do() override
+        {
+            _directories.Push (_subject._root); _stack_load++;
+            while (! _directories.Empty ())
+                H3R_NS::OS::EnumFiles<EnumTask> (*this,
+                    (_current_root = _directories.Pop ()).AsZStr ().Data (),
+                    [](EnumTask & task, const char * name, bool dir) -> bool
+                    {
+                        //TODO there is too much unwanted access here
+                        task.State.FileName = String {name};
+                        task.State.Name = task._current_root
+                            + H3R_PATH_SEPARATOR + String {name};
+                        task.State.IsDirectory = dir;
+                        task.State.Progress++;
+                        if (task.State.IsDirectory)
+                            task._directories.Push (task.State.Name),
+                            task._stack_load =
+                                task._directories.Size () > task._stack_load ?
+                                task._directories.Size () : task._stack_load;
+                    return task._subject.ThrNotify ();
+                });
+            H3R_NS::OS::Log_stdout ("stack_load: %zu" EOL, _stack_load);
+            _complete = true;
+        }
+        public bool Complete() const { return _complete; }
+    } _task;
+    public bool Complete() const { return _task.Complete (); }
+};// AsyncFsEnum
+
+#undef private
+#undef public
+
+NAMESPACE_H3R
+#endif
