10dd271b8d160df1de900ae043408fd615f2f95e
corwinn <corwinn@users.noreply.github.com>
2023-02-02 19:15:26 +0200

    parse_def: update and a fix

--- parse_def.cpp
+++ parse_def.cpp
@@ -34,7 +34,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // Not for the faint of heart. This is quick'n'dirty code for FF analysis.
 
-//c clang++ -std=c++11 -fsanitize=address,undefined,integer -fvisibility=hidden -I. -Ios -Ios/posix -Iutils -Istream -O0 -g -DH3R_DEBUG -fno-exceptions -fno-threadsafe-statics main.a h3r_game.o parse_def.cpp -o parse_def
+//c clang++ -std=c++11 -DH3R_MM -fsanitize=address,undefined,integer,leak -fvisibility=hidden -I. -Ios -Ios/posix -Iutils -Istream -O0 -g -DH3R_DEBUG -fno-exceptions -fno-threadsafe-statics main.a parse_def.cpp -o parse_def
 
 #define int int
 static_assert(4 == sizeof(int), "define 32-bit int type");
@@ -45,7 +45,6 @@ H3R_ERR_DEFINE_HANDLER(Memory,H3R_ERR_HANDLER_UNHANDLED)
 H3R_ERR_DEFINE_HANDLER(Log,H3R_ERR_HANDLER_UNHANDLED)
 
 #include "h3r_filestream.h"
-#include "h3r_game.h"
 
 H3R_NAMESPACE
 namespace OS {
@@ -85,7 +84,7 @@ static struct { // takes care of the render to atlas part
     bool _bonus_sprite {};
     int _block {};
     int _sprite {};
-    H3R_NS::Array<H3R_NS::byte> _b;
+    H3R_NS::Array<H3R_NS::byte> * _b {};
     H3R_NS::byte _digits[10*GH] = { // my font
      0x38, 0x44, 0x82, 0x82, 0x92, 0x82, 0x82, 0x44, 0x38,
      0x10, 0x30, 0x50, 0x90, 0x10, 0x10, 0x10, 0x10, 0xFE,
@@ -111,7 +110,7 @@ static struct { // takes care of the render to atlas part
     H3R_NS::byte _semicolon[GH] = {0, 0, 16, 0, 0, 0, 16, 0, 0}; // my font
     inline void put_pixel(int x, int y, int cidx)
     {
-        *((H3R_NS::byte *)_b + y * _w + x) = cidx;
+        *((H3R_NS::byte *)*_b + y * _w + x) = cidx;
     }
     inline void RenderGlyph(const H3R_NS::byte * g, int x, int y, int cidx)
     {//TODONT optimize :)
@@ -143,30 +142,34 @@ static struct { // takes care of the render to atlas part
     void Add(const H3R_NS::byte * b, int w, int h, int l, int t)
     {
         if (_x >= _columns) {
-            //TODO "Not allocated here" related to Array.Resize ()
-            _b.Resize (_b.Length () + _w * _row_height);
+            //DONE "Not allocated here" related to Array.Resize ()
+            // perhaps the array gets released after the MM due to "bmp" being
+            // static. Ensuring it gets released prior main() ends, resolves the
+            // issue.
+            // printf ("resize to: %lu" EOL, _b->Length () + _w * _row_height);
+            _b->Resize (_b->Length () + _w * _row_height);
             _x = 0, _y++, _h += _row_height;
         }
         int left = _x * _column_width, top = _y * _row_height;
-        // H3R_NS::OS::Log_stdout ("Add: _x: %d, _y: %d, _w: %d, _h: %d"
+        // printf ("Add: _x: %d, _y: %d, _w: %d, _h: %d"
         //    ", left: %d, top: %d, w: %d, h: %d" EOL,
         //    _x, _y, _w, _h, left, top, w, h);
         for (int i = 0; i < h; i++)
             H3R_NS::OS::Memmove (
-                (H3R_NS::byte *)_b + ((top + i + t) * _w) + left + l,
+                (H3R_NS::byte *)*_b + ((top + i + t) * _w) + left + l,
                 b + i * w, w);
         int tw = RenderNum (_block, left + 5, top + 5, 255);
         RenderGlyph (_semicolon, left + 5 + tw, top + 5, 255), tw += GW;
         RenderNum (_sprite, left + 5 + tw, top + 5, 255);
         if (_bonus_sprite)
             for (int i = 0; i < _column_width; i++)
-                *((H3R_NS::byte *)_b + (top * _w) + i + _x * _column_width)
+                *((H3R_NS::byte *)*_b + (top * _w) + i + _x * _column_width)
                 = 255;
         _x++;
     }
     void Write(char * fn, const H3R_NS::byte * p)
     {
-        store_as_bmp (fn, _b.Data (), _w, _h, 8, p);
+        store_as_bmp (fn, _b->Data (), _w, _h, 8, p);
     }
 } bmp; // yes - it is this simple
 
@@ -202,17 +205,17 @@ int main(int argc, char ** argv)
     if (2 != argc)
         return printf ("usage: parse_def deffile\n");
 
-    H3R_NS::Game game;
-    game.SilentLog (true);
+    H3R_NS::Array<H3R_NS::byte> bmp_b {};
+    bmp._b = &bmp_b;
 
     H3R_NS::OS::FileStream s {argv[1], H3R_NS::OS::FileStream::Mode::ReadOnly};
-    H3R_NS::OS::Log_stdout (EOL "%s 1st offset: %8X" EOL, argv[1], s.Tell ());
+    printf (EOL "%s 1st offset: %8X" EOL, argv[1], (uint)s.Tell ());
 
     // named "Entry"?
     int type, w, h, bcnt; // bcnt - "group" count if you ask me
     Read (s, &type).Read (s, &w).Read (s, &h).Read (s, &bcnt);
 
-    H3R_NS::OS::Log_stdout (
+    printf (
         "Type: %d" EOL "Width: %3d" EOL "Height: %3d" EOL "Block count: %d" EOL,
         type, w, h, bcnt);
     bmp.Init (w, h);
@@ -228,20 +231,20 @@ int main(int argc, char ** argv)
         s.Seek (+4); // unknown1
         int cnt2; // some other count?
         Read (s, &cnt2);
-        H3R_NS::OS::Log_stdout ("Block %2d, sprite count: %d" EOL, i, cnt2);
+        printf ("Block %2d, sprite count: %d" EOL, i, cnt2);
         s.Seek (+4); // unknown2
         s.Seek (+4); // unknown3
         // EntryBlock.Data
         for (int j = 0; j < cnt2; j++) {
             H3R_NS::Array<H3R_NS::byte> name {14}; // pcx
             Read (s, (H3R_NS::byte *)name, 13);
-            H3R_NS::OS::Log_stdout (
+            printf (
                 "  name[%d]: \"%s\"" EOL, j, (char *)(H3R_NS::byte *)name);
         }
         for (int j = 0; j < cnt2; j++) {
             int offset; // 0-based
             Read (s, &offset);
-            H3R_NS::OS::Log_stdout (
+            printf (
                 "  offset[%d]: %8X" EOL, j, offset);
             auto sentinel = s.Tell ();
             // so its not an entry - its a sprite
@@ -252,7 +255,7 @@ int main(int argc, char ** argv)
         }
         //TODO the above is an odd way to store data - figure out why
     } // for (int i = 0, ecnt = 0; i < bcnt; i++)
-    H3R_NS::OS::Log_stdout (" offset after all blocks: %8X" EOL, last_ofs);
+    printf (" offset after all blocks: %8X" EOL, (uint)last_ofs);
 
     // There are a lot of .def files containing series of name-less sprites
     // after the "main" data:
@@ -266,20 +269,20 @@ int main(int argc, char ** argv)
     if (last_ofs != s.Size ()) {
         bmp._bonus_sprite = true;
         bmp._block = bmp._sprite = -1;
-        // H3R_NS::OS::Log_stdout ("unparsed" EOL);
+        // printf ("unparsed" EOL);
         auto sz = s.Size (); // just in case
         s.Seek (last_ofs - s.Tell ());
         while (s.Tell () < sz) {
-            H3R_NS::OS::Log_stdout ("Bonus sprite #%d" EOL, ++k);
+            printf ("Bonus sprite #%d" EOL, ++k);
             ++bmp._sprite, read_sprite (s);
             last_ofs = s.Tell ();
         }
     }
     if (last_ofs != s.Size ())
-        H3R_NS::OS::Log_stdout (
+        printf (
             "unparsed2: pos: %8X, size: %8X, bonus sprites: %d" EOL,
-            last_ofs, s.Size (), k);
-    else H3R_NS::OS::Log_stdout ("sprites: %d, bonus sprites: %d" EOL,
+            (uint)last_ofs, (uint)s.Size (), k);
+    else printf ("sprites: %d, bonus sprites: %d" EOL,
         scnt, k);
 
     bmp.Write (argv[1], palette.Data ());
@@ -290,13 +293,13 @@ int main(int argc, char ** argv)
 static void read_sprite_224(H3R_NS::Stream & s, int w, int h, H3R_NS::byte * p)
 {
     for (int i = 0; i < h; i++) { // for each line
-        // H3R_NS::OS::Log_stdout ("Line #%2d" EOL, i);
+        // printf ("Line #%2d" EOL, i);
         for (int llen = 0; llen < w; ) {
             H3R_NS::byte b, len;
-            // H3R_NS::OS::Log_stdout ("  ofs:  %8X:", s.Tell ());
+            // printf ("  ofs:  %8X:", s.Tell ());
             Read (s, &b);
             len = (b & 31) + 1;
-            // H3R_NS::OS::Log_stdout (" b: %2X, v: %3d, len: %3d, llen: %3d"
+            // printf (" b: %2X, v: %3d, len: %3d, llen: %3d"
             //    EOL, b, b>>5, len, llen + len);
             if ((224 & b) == 224) s.Read (p + i * w + llen, len);
             else for (int j = 0; j < len; j++) *(p + i * w + llen + j) = b>>5;
@@ -305,7 +308,7 @@ static void read_sprite_224(H3R_NS::Stream & s, int w, int h, H3R_NS::byte * p)
             llen += len;
         }
     }
-    // H3R_NS::OS::Log_stdout (
+    // printf (
     //    " offset after 1st sprite: %8X" EOL, s.Tell ());
     // H3R_NS::OS::Exit (1);
 }
@@ -324,7 +327,7 @@ void read_sprite(H3R_NS::Stream & s)
 
     Read (s, &size).Read (s, &type).Read (s, &aw).Read (s, &ah).
     Read (s, &w).Read (s, &h).Read (s, &l).Read (s, &t);
-    H3R_NS::OS::Log_stdout (
+    printf (
         "   size: %4d: type: %1d, aw: %3d, ah: %3d, w: %3d, h: %3d, l: %2d,"
         " t: %2d" EOL, size, type, aw, ah, w, h, l, t);
     // type1: 32524 sprites
@@ -340,13 +343,13 @@ void read_sprite(H3R_NS::Stream & s)
         auto off_table_base = s.Tell ();
         Read (s, (unsigned int *)off_tbl, off_tbl.Length ());
         for (int i = 0; i < h; i++) {
-            // H3R_NS::OS::Log_stdout ("Line #%2d" EOL, i);
+            // printf ("Line #%2d" EOL, i);
             // s.Seek (off_table_base - s.Tell ()).Seek (off_tbl[i]);
             for (int llen = 0; llen < w; ) {
-                // H3R_NS::OS::Log_stdout ("  ofs:  %8X:", s.Tell ());
+                // printf ("  ofs:  %8X:", s.Tell ());
                 H3R_NS::byte b, len;
                 Read (s, &b).Read (s, &len);
-                // H3R_NS::OS::Log_stdout (" b: %2X, len: %3d, llen: %3d"
+                // printf (" b: %2X, len: %3d, llen: %3d"
                 //    EOL, b, len + 1, llen + len + 1);
                 if (255 == b) s.Read (ptr + i * w + llen, len + 1);
                 else for (int j = 0; j < (len + 1); j++)
@@ -367,7 +370,7 @@ void read_sprite(H3R_NS::Stream & s)
             "sprite type 3: the 1st offset doesn't match table length")
         read_sprite_224 (s, w, h, ptr);
         /*for (int i = 0; i < h; i++) { // for each line
-            // H3R_NS::OS::Log_stdout ("Line #%2d" EOL, i);
+            // printf ("Line #%2d" EOL, i);
             // s.Seek (off_table_base - s.Tell ()).Seek (off_tbl[ofs_num*i]);
             // only the 1st offset is being used?!
             // off_tbl[3*i+0] - the 1st enc. byte of the current line
@@ -400,18 +403,18 @@ void read_sprite(H3R_NS::Stream & s)
                 //if (off_table_base + off_tbl[ofs_num*i + k] == s.Tell ())
                 //   { ofs_id = k + 1; break; }
                 //if (ofs_id)
-                //    H3R_NS::OS::Log_stdout ("  ofs: %d%8X:", ofs_id, s.Tell ());
-                //else H3R_NS::OS::Log_stdout ("  ofs:  %8X:", s.Tell ());
+                //    printf ("  ofs: %d%8X:", ofs_id, s.Tell ());
+                //else printf ("  ofs:  %8X:", s.Tell ());
                 Read (s, &b);
                 len = (b & 31) + 1;
-                //H3R_NS::OS::Log_stdout (" b: %2X, v: %3d, len: %3d, llen: %3d"
+                //printf (" b: %2X, v: %3d, len: %3d, llen: %3d"
                 //    EOL, b, b>>5, len, llen + len);
                 if ((224 & b) == 224) s.Seek (len); // copy "len" bytes
                 else ; // "b>>5" repeats "len" times
                 llen += len;
             }
         } // for (int i = 0; i < h; i++)
-            // H3R_NS::OS::Log_stdout (
+            // printf (
             //    " offset after 1st sprite: %8X" EOL, s.Tell ());
             // H3R_NS::OS::Exit (1);*/
     } // if (3 == ftype)
@@ -427,7 +430,7 @@ void read_sprite(H3R_NS::Stream & s)
             //s.Seek (w); // read "w" pixels
     } // if (0 == type)
     else {
-        H3R_NS::OS::Log_stdout ("Unknown type%d " EOL, type);
+        printf ("Unknown type%d " EOL, type);
         H3R_NS::OS::Exit (1);
     }
 
