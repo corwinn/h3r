c8231f80ea8e90bc813d71bff0e5fc81fd4bf4d0
corwinn <corwinn@users.noreply.github.com>
2023-02-26 13:39:49 +0200

    dbg: singleton

--- utils/h3r_dbg.cpp
+++ utils/h3r_dbg.cpp
@@ -32,45 +32,13 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 **** END LICENCE BLOCK ****/
 
-#ifndef _H3R_DBG_H_
-#define _H3R_DBG_H_
-
-#include "h3r.h"
-#include "h3r_os.h"
-#include "h3r_string.h"
+#include "h3r_dbg.h"
 
 H3R_NAMESPACE
 
-// Convenience.
-struct UnqueuedThreadSafeDebugLog final
+UnqueuedThreadSafeDebugLog & UnqueuedThreadSafeDebugLog::D()
 {
-    using L = UnqueuedThreadSafeDebugLog;
-    template <typename T> inline L & Fmt(const char * f, T & v)
-    {
-        if (Enabled) OS::Log_stdout (f, v);
-        return *this;
-    }
-    template <typename T> inline L & Fmt(const char * f, T && v)
-    {
-        if (Enabled) OS::Log_stdout (f, v);
-        return *this;
-    }
-    inline L & operator<<(const char * v) { return Fmt ("%s", v); }
-    inline L & operator<<(long v) { return Fmt ("%ld", v); }
-    inline L & operator<<(unsigned long v) { return Fmt ("%lu", v); }
-    inline L & operator<<(int v) { return Fmt ("%d", v); }
-    inline L & operator<<(short v) { return Fmt ("%d", v); }
-    inline L & operator<<(byte v) { return Fmt ("%d", v); }
-    inline L & operator<<(const String & v) { return Fmt ("%s", v.AsZStr ()); }
-    // Dbg << Dbg.Fmt ("%foo", bar) << "p" << q << EOL;
-    inline L & operator<<(L & l) { return l; }
-
-    bool Enabled {true};
-    static UnqueuedThreadSafeDebugLog & D();
-};
-
-#define Dbg (H3R_NS::UnqueuedThreadSafeDebugLog::D ())
+    static UnqueuedThreadSafeDebugLog l; return l;
+}
 
 NAMESPACE_H3R
-
-#endif
