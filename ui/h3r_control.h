15c62cabd0c7f95039273ced807953c228478686
corwinn <corwinn@users.noreply.github.com>
2023-02-28 13:48:10 +0200

    control: state++: hidden

--- ui/h3r_control.h
+++ ui/h3r_control.h
@@ -64,67 +64,83 @@ H3R_NAMESPACE
 // necessity, is -Pos();
 class Control
 {
-    protected: using Window = class Window;
+#define public public:
+#define private private:
+#define protected protected:
+
+    protected using Window = class Window;
     friend Window;
 
-    private: Box _bb; // bounding box
-    private: bool _enabled {true}; // whether the user can interact with it
+    private Box _bb; // bounding box
+    private bool _enabled {true}; // whether the user can interact with it
+    private bool _hidden {false}; // whether the user can interact with it
+    // The difference: _enabled represents whether a control is "grayed out";
+    // _hidden controls wheter a control is rendered at all.
 
-    protected: Window * _window;
-    protected: h3rDepthOrder _depth;
+    protected Window * _window;
+    protected h3rDepthOrder _depth;
     // Render order. See Window: "Automatic depth".
-    protected: inline virtual h3rDepthOrder Depth() { return _depth; }
-    private: List<Control *> _n;   // Sub-controls
-    private: void Add(Control *);
+    protected inline virtual h3rDepthOrder Depth() { return _depth; }
+    private List<Control *> _n;   // Sub-controls
+    private void Add(Control *);
 
     // Handle nullptr at con. init.
-    protected: static Window * GetWindow(Control *);
+    protected static Window * GetWindow(Control *);
 
     // Controls placed directly on another control have the same depth
     // (control+1); their rendering order is the order of placement.
-    public: Control(Control *); // sets _depth
+    public Control(Control *); // sets _depth
     // Controls placed directly on a window have the same depth (window+1);
     // their rendering order is the order of placement.
-    public: Control(Window *);  // sets _depth
-    public: virtual ~Control() {}
+    public Control(Window *);  // sets _depth
+    public virtual ~Control() {}
 
-    public: const Point & Pos() { return _bb.Pos; }
-    public: const Point & Size() { return _bb.Size; }
+    public const Point & Pos() { return _bb.Pos; }
+    public const Point & Size() { return _bb.Size; }
     // Let controls know, their position has been changed.
-    public: virtual Control * SetPos(int, int);
+    public virtual Control * SetPos(int, int);
 
     // Upload the Frames to the RenderEngine.
     // This is not called on construction, on purpose: so that layout can be
     // done prior uploading to the render engine.
-    public: inline virtual void UploadFrames(/*RenderEngine * = nullptr*/) {}
+    public inline virtual void UploadFrames(/*RenderEngine * = nullptr*/) {}
 
-    // properties
-    public: void SetEnabled(bool value) { _enabled = value; }
-    public: bool Enabled() { return _enabled; } // Has a specific sprite for it
+    //TODO template <typename T, typename Changed> class Property.
+    public inline void SetEnabled(bool value) { _enabled = value; }
+    // Has a specific sprite for it.
+    public inline bool Enabled() const { return _enabled; }
+    public inline void SetHidden(bool value)
+    {
+        if (value != _hidden) {
+            _hidden = value;
+            OnVisibilityChanged ();
+        }
+    }
+    // "hidden" do receive no events.
+    public inline bool Hidden() const { return _hidden; }
 
-    protected: inline Box & ClientRectangle() { return _bb; }
+    protected inline Box & ClientRectangle() { return _bb; }
     //LATER Allowed for now. Its bound to its sprite. Scaling shall be done by
     //      the render engine regardless of this function here.
-    protected: void Resize(int, int);
-    protected: virtual bool HitTest(Point & p);
+    protected void Resize(int, int);
+    protected virtual bool HitTest(Point & p);
 
-    // Do not check _visible here; there is visible list and a non-visible one -
-    // controlled by event-driven logic; no need to check visibility each time
-    // render is issued.
-    // No UI editor so these remain empty.
-    //TODO figure out a way for the subject(window) to notify its observers
-    //     w/o publishing these methods for everyone to call
-    public: virtual void OnRender(/*GC &*/) {}
-    public: virtual void OnMouseMove(const EventArgs &) {}
-    public: virtual void OnMouseUp(const EventArgs &) {}
-    public: virtual void OnMouseDown(const EventArgs &) {}
-    public: virtual void OnKeyDown(const EventArgs &) {}
-    public: virtual void OnKeyUp(const EventArgs &) {}
+    protected virtual void OnRender(/*GC &*/) {} //TODO useless?
+    protected virtual void OnMouseMove(const EventArgs &) {}
+    protected virtual void OnMouseUp(const EventArgs &) {}
+    protected virtual void OnMouseDown(const EventArgs &) {}
+    protected virtual void OnKeyDown(const EventArgs &) {}
+    protected virtual void OnKeyUp(const EventArgs &) {}
+    protected virtual void OnVisibilityChanged() {}
 
     //TODO hint system; MyHint: Event ... show_hint (myhint);
     //     Control.RMB += (Event & ShowHint) => Root().OnEvent (ShowHint);
     //     After merging CoR back into the mcast delegate.
     // public: virtual void OnEvent(Event &);
+
+#undef public
+#undef private
+#undef protected
 };
 
 NAMESPACE_H3R
